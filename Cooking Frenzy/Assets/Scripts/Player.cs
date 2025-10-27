e8622d78085950a390ce428f29551813f2e54cdd
PaulaCab <paula.cab.car@gmail.com>
2025-10-27 22:17:16 +0100

    [Add] Set up collisions

--- Cooking Frenzy/Assets/Scripts/Player.cs
+++ Cooking Frenzy/Assets/Scripts/Player.cs
@@ -13,7 +13,37 @@ public class Player : MonoBehaviour
     {
         Vector3 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 dir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += dir * moveSpeed * Time.deltaTime;
+
+        float playerRadius = .8f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight,
+                                            playerRadius, dir, moveSpeed*Time.deltaTime);
+        
+        //If can't move dyagonal try separate axis
+        if (!canMove)
+        {
+            //Attemp only X movement
+            Vector3 dirX = new Vector3(dir.x, 0, 0).normalized;
+            canMove= !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight,
+                                            playerRadius, dirX, moveSpeed*Time.deltaTime);
+
+            if (canMove)
+                dir = dirX;
+            else
+            {
+                //Attemp only Z movement
+                Vector3 dirZ = new Vector3(0, 0, dir.z).normalized;
+                canMove= !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight,
+                    playerRadius, dirZ, moveSpeed*Time.deltaTime);
+                
+                if (canMove)
+                    dir = dirZ;
+            }
+        }
+
+        if(canMove)
+            transform.position += dir * moveSpeed * Time.deltaTime;
+        
         transform.forward= Vector3.Slerp(transform.forward,dir, Time.deltaTime*10);
 
         isWalking = dir != Vector3.zero;
